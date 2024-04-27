db28f05f5b9e600b232f6bb8caf960e225a0c894
Alyssa Dwiana Mulia <144673468+AlyssaMulia@users.noreply.github.com>
2024-04-27 14:14:17 +0800

    Update Wisata.py

--- Model/Wisata.py
+++ Model/Wisata.py
@@ -1,7 +1,22 @@
 class Wisata: #Node untuk menyimpan data tempat wisata
-    def __init__(self, data):
-        self.data = {"id_wisata"   : data[0],
-                    "nama_wisata" : data[1],
-                    "deskripsi"  : data[2],
-                    "lokasi" : data[3]} 
+    def __init__(self, id_wisata, nama_wisata, deskripsi, lokasi):
+        self.id_wisata = id_wisata
+        self.nama_wisata = nama_wisata
+        self.deskripsi = deskripsi
+        self.lokasi = lokasi
         self.next = None
+        self.previous = None
+
+class ListWisata:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.history = []
+
+    def __len__(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
